f3e4b7ce5fddb6b4b88088e7b9ff4b0f31fe36a5
feelonewong <feelonewong@gmail.com>
2021-04-14 17:49:09 +0800

    增加无效接口提醒404 NOT FOUND &&  格式化path

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const handleUserRouter = require("./src/router/user");
 const handleBlogRouter = require("./src/router/blog");
 const serverHandle = (req, res) => {
   res.setHeader("Content-type", "application/json");
+  const { url } = req;
+  req.path = url.split("?")[0];
 
   const userData = handleUserRouter(req, res);
   if (userData) {
@@ -13,6 +15,11 @@ const serverHandle = (req, res) => {
     res.end(JSON.stringify(blogData));
     return;
   }
+
+  //处理未找到路由的接口
+  res.writeHead(404, {"Content-type":"text/plain;charset=utf-8"});
+  res.write("404 NOT FOUND");
+  res.end();
 };
 
 module.exports = serverHandle;
--- src/router/user.js
+++ src/router/user.js
@@ -1,9 +1,8 @@
 const handleUserRouter = (req, res) => {
   const { method } = req;
-  const { url } = req;
-  const path = url.split("?")[0];
+  
 
-  if (method === "POST" && path === "/api/user/login") {
+  if (method === "POST" && req.path === "/api/user/login") {
     return {
       data: "这是登录接口",
     };
