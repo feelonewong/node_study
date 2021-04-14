e9078e591ad3a5fd98ab8c2b132e88a72893dd3e
feelonewong <feelonewong@gmail.com>
2021-04-14 15:44:31 +0800

    手写http post请求

--- app.js
+++ app.js
@@ -1,20 +1,30 @@
 const http = require("http");
-const PORT  = 8088;
+const PORT = 8088;
 const querystring = require("querystring");
 
-const server = http.createServer( (req, res)=>{
-    const {url} = req;
-    req.query  = querystring.parse( url.split("?")[1] )
+const server = http.createServer((req, res) => {
+  const { method } = req;
+  if (method === "POST") {
+    let postdata = "";
+    req.on("data", (chunk) => {
+      postdata += chunk.toString();
+    });
 
-    res.writeHeader(200, { "Content-type":"text/html;charset=utf-8" })
+    req.on("end", () => {
+      res.end(postdata);
+    });
+  } else if (method === "GET") {
+    const { url } = req;
+    req.query = querystring.parse(url.split("?")[1]);
+    res.writeHeader(200, { "Content-type": "text/html;charset=utf-8" });
     res.end(JSON.stringify(req.query));
-} )
+  }
+});
 
-
-server.listen( PORT, (err)=>{
-    if(err){
-        console.log(err);
-    }else{
-        console.log(`http://localhost:${PORT}`);
-    }
-} )
+server.listen(PORT, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(`http://localhost:${PORT}`);
+  }
+});
