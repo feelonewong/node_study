8b1b9f0937e962f96f3c4a43b7d4472b52717107
feelonewong <feelonewong@gmail.com>
2021-04-15 17:29:25 +0800

    添加promise测试

--- promise-test/index.js
+++ promise-test/index.js
@@ -1,26 +1,48 @@
 const path = require("path");
 const fs = require("fs");
 
-function getFileContent(filename, callback) {
-  const fullName = path.resolve(__dirname, "./files", filename);
+// function getFileContent(filename, callback) {
+//   const fullName = path.resolve(__dirname, "./files", filename);
+//   fs.readFile(fullName, (err, data) => {
+//     if (err) {
+//       console.err(err);
+//     }
+//     callback(
+//         JSON.parse(data.toString())
+//     )
+//   });
+// }
 
-  fs.readFile(fullName, (err, data) => {
-    if (err) {
-      console.err(err);
-    }
-    callback(
-        JSON.parse(data.toString())
-    )
+// getFileContent( "a.json", (cb)=>{
+//     console.log(cb);
+//     getFileContent(cb.next, bData=>{
+//         console.log(bData);
+//         getFileContent( bData.next, (cData)=>{
+//             console.log(cData);
+//         })
+//     });
+// })
+function getFileContent(filename) {
+  return new Promise((resolve, reject) => {
+    const fullName = path.resolve(__dirname, "./files", filename);
+    fs.readFile(fullName, (err, data) => {
+      if (err) {
+        reject(err);
+      }
+      resolve(JSON.parse(data.toString()));
+    });
   });
 }
 
-getFileContent( "a.json", (cb)=>{
-    console.log(cb);
-    getFileContent(cb.next, bData=>{
-        console.log(bData);
-        getFileContent( bData.next, (cData)=>{
-            console.log(cData);
-        })
-    });
-
-})
+getFileContent("a.json").then( (res)=>{
+    console.log(res);
+    return getFileContent(res.next);
+}).then( (res)=>{
+    console.log(res);
+    return getFileContent(res.next)
+}).then( (res)=>{
+    console.log(res);
+    
+}).catch( error=>{
+    console.err(error);
+} )
