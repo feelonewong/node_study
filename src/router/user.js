2dd87310a8f9ff440cc61c2f8ed7f6785e99b85e
feelonewong <feelonewong@gmail.com>
2021-04-18 16:35:06 +0800

    设置cookie过期时间

--- src/router/user.js
+++ src/router/user.js
@@ -1,5 +1,12 @@
 const { SuccessModel, ErrorModel } = require("../model/resData");
 const { loginCheck  } = require("../controller/user");
+
+const getCookieExpires = ()=>{
+  const d  = new Date();
+  d.setTime(d.getTime() + (24*60*60*1000) );
+  return d.toGMTString();
+}
+
 const handleUserRouter = (req, res) => {
   const { method } = req;
   if (method === "POST" && req.path === "/api/user/login") {
@@ -11,7 +18,7 @@ const handleUserRouter = (req, res) => {
         return new ErrorModel("登录失败");
       }
       if(data[0].username){
-        res.setHeader("Set-Cookie",`username=${username};path=/;httpOnly`)
+        res.setHeader("Set-Cookie",`username=${username};path=/;httpOnly;expires=${getCookieExpires}`)
         return new SuccessModel(true);
       }
       
